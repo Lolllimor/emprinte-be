4986a9108ab6285bd2b69735111da342c9a30eb4
Lollimor <etap-09@etap-09s-MacBook-Air.local>
2025-11-07 23:05:53 +0100

    feat: import export strapi plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,8 @@
 export default () => ({
   'strapi-csv-import-export': {
     config: {
-      authorizedExports: ['api::user.user',"api::emails.email"],
-      authorizedImports: ['api::user.user',"api::emails.email"],
+      authorizedExports: ['api::user.user',"api::email.email"],
+      authorizedImports: ['api::user.user',"api::email.email"],
     },
   },
 });
